b328fd9989cd85ed454fadf0132e9e0dd037722d
Hostek <michalpl245674477@gmail.com>
2025-03-06 18:46:38 +0100

    add roboto-mono font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<main class="content roboto-font">
+sadf
+</main>
+
+<style lang="scss">
+
+</style>
