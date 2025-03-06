44c7bc438e67c84a3375223d02e19b9792bfd715
Hostek <michalpl245674477@gmail.com>
2025-03-06 20:07:07 +0100

    better responsivness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,5 +15,16 @@
     main {
         display: grid;
         grid-template-columns: repeat(12, 1fr);
+        font-size: 1.3vw;
+
+        @media only screen and (max-width: 1000px) {
+            grid-template-columns: repeat(6, 1fr);
+            font-size: 2.6vw;
+        }
+
+        @media only screen and (max-width: 500px) {
+            grid-template-columns: repeat(3, 1fr);
+            font-size: 5.2vw;
+        }
     }
 </style>
